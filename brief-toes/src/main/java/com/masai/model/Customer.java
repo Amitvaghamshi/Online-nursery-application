5cab4e86b81764be2c26c5192e24b544c1b25142
amitamitvaghamshi9@gmail.comamitgit config --global user.email amitvaghamshi9@gmail.com <amitvaghamshi9@gmail.comgit config --global user.name amit>
2023-02-22 08:35:05 +0530

    start

--- brief-toes/src/main/java/com/masai/model/Customer.java
+++ brief-toes/src/main/java/com/masai/model/Customer.java
@@ -2,4 +2,10 @@ package com.masai.model;
 
 public class Customer {
 
+	   private Integer customerId;
+	   private String customerName;
+	   private String customerEmail;
+	   private String username;
+	   private String password;
+	   //private Address A
 }
