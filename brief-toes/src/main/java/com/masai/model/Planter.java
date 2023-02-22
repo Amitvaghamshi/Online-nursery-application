aaacd6d65f1d46a944a52c2abe635d0c91101320
Chanchal Soni <chanchalsoni1998@gmail.com>
2023-02-22 12:22:04 +0530

    planter.java and service

--- brief-toes/src/main/java/com/masai/model/Planter.java
+++ brief-toes/src/main/java/com/masai/model/Planter.java
@@ -20,34 +20,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Planter {
 
-		@Id
-		@GeneratedValue(strategy = GenerationType.AUTO)	
-		private Integer planterId;
-		
-		@Min(value=2 , message = "Planter height should be more than 2")
-		private Double planterHeight;
-		
-		@Min(value=2 , message = "Planter capacity cannot be less than 2")
-		private Double planterCapacity;
-		
-		@Min(value=5 , message = "Drainage holes should not be less than 5")
-		private Integer drainageHoles;
-		
-		@NotNull
-		private String planterColor;
-		
-		@NotNull
-		private String planterShape;
-		
-		@Min(value=1 , message = "Planter stock should be atleast 1")
-		private Integer planterStock;
-		
-		@Size(min=30,max=500, message = "Planter cost is between 30 to 500")
-		private Integer planterCost;
-		
-		Seed seed;
-		
-		Plant plant;
+
 
 
 }
--- brief-toes/src/main/java/com/masai/repository/PlanterRepository.java
+++ brief-toes/src/main/java/com/masai/repository/PlanterRepository.java
@@ -1,5 +1,22 @@
 package com.masai.repository;
 
-public interface PlanterRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.masai.model.Plant;
+import com.masai.model.Planter;
+import com.masai.model.Seed;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public interface PlanterRepository extends JpaRepository<Planter, Integer> {
+	
+
+	
 }
