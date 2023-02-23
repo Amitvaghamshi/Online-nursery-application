d85e831b1d4d5848ca566a99db54b89d1b534f61
Umair Ali <86684813+umair8574@users.noreply.github.com>
2023-02-23 21:23:15 +0530

    day3final

---
+++ brief-toes/src/main/java/com/masai/exceptions/PlantException.java
@@ -0,0 +1,12 @@
+package com.masai.exceptions;
+
+public class PlantException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+public PlantException(){}
+	
+	public PlantException(String message){
+		super(message);
+	}
+
+}
