4ac4d4a73cb4f31935bc4c2ce85aa85c03422722
Chanchal Soni <chanchalsoni1998@gmail.com>
2023-02-23 20:30:11 +0530

    day3

--- brief-toes/src/main/java/com/masai/service/CustomerService.java
+++ brief-toes/src/main/java/com/masai/service/CustomerService.java
@@ -1,6 +1,13 @@
 package com.masai.service;
 
+import java.util.List;
+
+import com.masai.exceptions.PlanterException;
+import com.masai.model.Customer;
+
 public interface CustomerService {
 
-	    //public List<Plants>
+	public Customer addCustomer(Customer customer) throws PlanterException;
+
+
 }
--- brief-toes/src/main/java/com/masai/service/CustomerServiceImpl.java
+++ brief-toes/src/main/java/com/masai/service/CustomerServiceImpl.java
@@ -1,5 +1,33 @@
 package com.masai.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.masai.exceptions.PlanterException;
+import com.masai.model.Customer;
+
+import com.masai.repository.CustomerRepository;
+
+@Service
 public class CustomerServiceImpl implements CustomerService{
+	
+	@Autowired
+	private  CustomerRepository customerRepo;
+
+	@Override
+	public Customer addCustomer(Customer customer) throws PlanterException {
+		
+		Customer newCustomer = customerRepo.save(customer);
+			
+		 if(newCustomer==null) throw new PlanterException("Can't add this newCustomer");
+			
+		 else return newCustomer;
+		
+
+	}
+
 
 }
